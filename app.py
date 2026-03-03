f7fbced020b1e185662affbb4d21e32e953a0569
teskio83 <64702032+teskio83@users.noreply.github.com>
2026-03-03 09:04:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -89,10 +89,6 @@ def init_db():
     except Exception as e:
         print("INIT DB ERROR:", e)
             
-@app.before_first_request
-def setup_database():
-    init_db()
-
 def as_obj(d):
     return SimpleNamespace(**d)
 
